4bf3569bd17c9ee6707cf94ee5a7911143f90f37
Thanh Thu Cao <thuctse63589@fpt.edu.vn>
2021-11-24 21:31:03 +0700

    add attachments to ApplicationDTO

--- common/src/main/java/ojt/management/common/payload/dto/ApplicationDTO.java
+++ common/src/main/java/ojt/management/common/payload/dto/ApplicationDTO.java
@@ -3,8 +3,10 @@ package ojt.management.common.payload.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ojt.management.data.entities.Attachment;
 
 import java.io.Serializable;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -17,4 +19,5 @@ public class ApplicationDTO implements Serializable {
     private boolean isSchoolDenied;
     private StudentDTO student;
     private JobDTO job;
+    private Set<Attachment> attachments;
 }
